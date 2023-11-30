360f4361a1ac36f30d4bdb2b62ae1cfad7b8a362
Niculescu Traian <traian30y@yahoo.co.uk>
2023-11-30 20:41:10 +0000

    update vectors e13

---
+++ src/bin/e13_vectors.rs
@@ -0,0 +1,91 @@
+// Topic: Vectors
+//
+// Requirements:
+// * Print 10, 20, "thirty", and 40 in a loop
+// * Print the total number of elements in a vector
+//
+// Notes:
+// * Use a vector to store 4 numbers
+// * Iterate through the vector using a for..in loop
+// * Determine whether to print the number or print "thirty" inside the loop
+// * Use the .len() function to print the number of elements in a vector
+
+use std::io;
+
+fn get_no_elements_value() -> u32 {
+    let mut value = String::new();
+    println!("input an integer value between 1 and 10: ");
+    loop {
+        io::stdin()
+            .read_line(&mut value)
+            .expect("failed to read line.");
+        let value: u32 = match value.trim().parse() {
+            Ok(num) => {
+                if num == 0 {
+                    println!("0 not allowed");
+                    value.clear();
+                    continue;
+                } else if num > 10 {
+                    println!("no more than 10");
+                    value.clear();
+                    continue;
+                } else {
+                    num
+                }
+            }
+            Err(_) => {
+                println!("only u32 allowed");
+                value.clear();
+                continue;
+            }
+        };
+        return value;
+    }
+}
+
+fn generate_vector(num: u32) -> Vec<u32> {
+    let mut new_vector: Vec<u32> = Vec::new();
+    for i in 1..num + 1 {
+        new_vector.push(i * 10);
+    }
+    new_vector
+}
+
+fn int_el_to_string(num: u32) -> String {
+    match num {
+        10 => return "ten".to_owned(),
+        20 => return "twenty".to_owned(),
+        30 => return "thirty".to_owned(),
+        40 => return "fourty".to_owned(),
+        50 => return "fifty".to_owned(),
+        60 => return "sixty".to_owned(),
+        70 => return "seventy".to_owned(),
+        80 => return "eighty".to_owned(),
+        90 => return "ninety".to_owned(),
+        100 => return "one hundred".to_owned(),
+        _ => return "error: input out of range".to_owned(),
+    }
+}
+
+// returns a vector of Strings based on an integer value
+fn transform_vector_to_string(vector: &Vec<u32>) -> Vec<String> {
+    let mut vector_strings: Vec<String> = Vec::new();
+    for i in vector {
+        vector_strings.push(int_el_to_string(*i));
+    }
+    vector_strings
+}
+
+fn print_vector(vec: &Vec<String>) {
+    for i in vec {
+        println!("{i}");
+    }
+}
+
+fn main() {
+    let num_elemets: u32 = get_no_elements_value();
+    let my_vector = generate_vector(num_elemets);
+    println!("{:?}", my_vector);
+    let transformed_vector = transform_vector_to_string(&my_vector);
+    print_vector(&transformed_vector);
+}
