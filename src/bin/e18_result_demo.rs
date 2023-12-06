ff69ab35b4c753185b6325022b9b3ce5beb34df6
Traian <traian30y@yahoo.co.uk>
2023-12-06 13:25:02 +0000

    e18 result demo

---
+++ src/bin/e18_result_demo.rs
@@ -0,0 +1,45 @@
+// working with Result - demo
+
+use std::io;
+
+#[derive(Debug)]
+enum MenuChoice {
+    MainMenu,
+    Start,
+    Quit,
+}
+
+fn get_choice(choice: &str) -> Result<MenuChoice, String> {
+    match choice {
+        "mainmenu" => return Ok(MenuChoice::MainMenu),
+        "start" => return Ok(MenuChoice::Start),
+        "quit" => return Ok(MenuChoice::Quit),
+        user_choice => return Err(user_choice.to_owned()),
+    }
+}
+
+fn get_input() -> String {
+    let mut input: String = String::new();
+    loop {
+        input.clear();
+        println!("enter your choice: mainmenu/start/quit:");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read line.");
+        match input.to_lowercase().trim() {
+            "" => {
+                println!("empty inputs not allowed.");
+                continue;
+            }
+            user_input => return user_input.to_owned(),
+        }
+    }
+}
+fn main() {
+    let input = get_input();
+    let my_choice: Result<MenuChoice, String> = get_choice(&input);
+    match my_choice {
+        Ok(choice) => println!("Your choice was: {:?}", choice),
+        Err(string) => println!("Your choice was incorrect: {:?}", string),
+    }
+}
