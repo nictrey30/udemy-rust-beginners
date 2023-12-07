a3399c2720fd36c29b071e50f1e57ac32f5d9d93
Traian <traian30y@yahoo.co.uk>
2023-12-07 00:13:31 +0000

    e18 result demo update

--- src/bin/e18_result_demo.rs
+++ src/bin/e18_result_demo.rs
@@ -37,14 +37,19 @@ fn get_input() -> String {
 }
 
 fn print_choice(choice: &MenuChoice) {
-    println!("choice -> {:?}", choice);
+    println!("choice: {:?}", choice);
+}
+
+fn pick_choice(input: &str) -> Result<(), String> {
+    // by using ?, get_choice instead of returning a Result, will automatically perform a match operation
+    // if the Result is an Ok variant, then the inner data of Ok will be returned
+    // if it is the Err variant the function will returl a Result Err variant, otherwise we get the inner data
+    let choice: MenuChoice = get_choice(input)?;
+    print_choice(&choice);
+    Ok(())
 }
 
 fn main() {
     let input = get_input();
-    let choice: Result<MenuChoice, _> = get_choice(&input);
-    match choice {
-        Ok(inner_choice) => print_choice(&inner_choice),
-        Err(e) => println!("error: {}", e),
-    }
+    pick_choice(&input).expect("non-existent choice");
 }
