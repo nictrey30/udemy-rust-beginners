69f55315aab3c8b484f0e58e034d58b46bab20a7
Traian <traian30y@yahoo.co.uk>
2023-12-13 13:23:26 +0000

    p1 Level 3 finished

--- src/bin/p1.rs
+++ src/bin/p1.rs
@@ -11,7 +11,7 @@
 // * A vector is the easiest way to store the bills at level 1, but a hashmap will be easier to work with at levels 2 and 3.
 // * Create a function just for retrieving user input, and reuse it throughout your program.
 // * Create your program starting at level 1. Once finished, advance to the next level.
-
+#![allow(dead_code, unused_variables, unused_imports)]
 use rand::Rng;
 use std::{collections::HashMap, io};
 
@@ -61,21 +61,6 @@ impl Bill {
 
     fn create_bill(name: Option<String>, amount: f64) -> Self {
         Self { name, amount }
-    }
-
-    fn update_bill_amount(&mut self, new_amount: f64) {
-        self.amount = new_amount;
-    }
-
-    fn update_bill_name(&mut self, name: Option<String>) {
-        match name {
-            Some(inner_name) => self.name = Some(inner_name),
-            None => (),
-        }
-    }
-
-    fn return_current_amount(&self) -> f64 {
-        self.amount
     }
 }
 
@@ -142,35 +127,144 @@ fn print_hashmap(hashmap: &HashMap<u32, Bill>) {
 }
 
 fn delete_bills(hashmap: &mut HashMap<u32, Bill>) {
-    if hashmap.len() > 0 {
-        let mut delete_option = continue_input("delete item");
-        let mut current_total_elements = hashmap.len();
-        print_hashmap(hashmap);
+    let mut delete_option = continue_input("delete item");
+    let mut current_total_elements = hashmap.len();
+    while current_total_elements > 0 && delete_option == true {
         println!("{} elements remaining", current_total_elements);
-        while current_total_elements > 0 && delete_option == true {
-            'outer: loop {
-                let delete_id = get_id();
+        'outer: loop {
+            let delete_id = get_id();
+            for key in hashmap.keys() {
+                if key == &delete_id {
+                    hashmap.remove(&delete_id);
+                    print_hashmap(hashmap);
+                    break 'outer;
+                }
+            }
+            println!("id not valid.please input ids only from the bills list.");
+        }
+        current_total_elements = hashmap.len();
+        if current_total_elements > 0 {
+            delete_option = continue_input("delete item");
+            if !delete_option {
+                print_hashmap(hashmap);
+            }
+        } else {
+            println!("bill list empty!");
+            return ();
+        }
+    }
+}
+
+fn clone_hashmap(hashmap: &HashMap<u32, Bill>) -> HashMap<u32, Bill> {
+    let mut edited_hashmap: HashMap<u32, Bill> = HashMap::new();
+    for (k, v) in hashmap {
+        let new_bill: Bill;
+        let new_name: Option<String> = match &v.name {
+            Some(inner_name) => Some(inner_name.to_owned()),
+            None => None,
+        };
+        new_bill = Bill::create_bill(new_name, v.amount);
+        edited_hashmap.insert(*k, new_bill);
+    }
+    edited_hashmap
+}
+
+fn edit_bills(hashmap: &HashMap<u32, Bill>) -> HashMap<u32, Bill> {
+    let mut edited_hashmap: HashMap<u32, Bill> = clone_hashmap(hashmap);
+    print_hashmap(&edited_hashmap);
+    loop {
+        match continue_input("edit item") {
+            true => 'inner: loop {
+                let edit_id = get_id();
+                let mut found = false;
                 for key in hashmap.keys() {
-                    if key == &delete_id {
-                        hashmap.remove(&delete_id);
-                        print_hashmap(hashmap);
-                        break 'outer;
+                    if key == &edit_id {
+                        found = true;
+                        edited_hashmap = edit_bill(edit_id, &edited_hashmap);
+                        print_hashmap(&edited_hashmap);
+                        match continue_input("edit item") {
+                            true => continue,
+                            false => return edited_hashmap,
+                        }
                     }
                 }
-                println!("id not valid.please input ids only from the bills list.");
-            }
-            current_total_elements = hashmap.len();
-            if current_total_elements > 0 {
-                delete_option = continue_input("delete item");
-                if !delete_option {
-                    print_hashmap(hashmap);
+                if !found {
+                    println!("incorrect id, please input only valid id's");
+                    continue 'inner;
                 }
-            } else {
-                println!("bill list empty!");
-                return ();
+            },
+            false => {
+                print_hashmap(&edited_hashmap);
+                return edited_hashmap;
             }
         }
     }
+}
+
+fn edit_bill(id: u32, hashmap: &HashMap<u32, Bill>) -> HashMap<u32, Bill> {
+    let current_bill = match hashmap.get(&id) {
+        Some(bill) => Bill {
+            name: match &bill.name {
+                Some(inner_name) => Some(inner_name.to_owned()),
+                None => None,
+            },
+            amount: bill.amount,
+        },
+        None => Bill {
+            name: None,
+            amount: 0.00,
+        },
+    };
+    let mut edited_hashmap: HashMap<u32, Bill> = clone_hashmap(hashmap);
+    println!("current bills:");
+    let value = edited_hashmap.entry(id).or_insert(Bill {
+        name: None,
+        amount: 0.00,
+    });
+    let new_name: Option<String>;
+    let new_amout: f64;
+    loop {
+        println!("edit name/amount of {:?}", current_bill);
+        println!("input-> name or amount:");
+        let choice = get_input(None);
+        match choice {
+            Ok(string) => match string.trim() {
+                "name" => {
+                    new_name = Bill::get_name();
+                    match new_name {
+                        Some(inner_name) => {
+                            *value = Bill::create_bill(Some(inner_name), value.amount);
+                        }
+                        None => {
+                            *value = Bill::create_bill(None, value.amount);
+                        }
+                    }
+                    break;
+                }
+                "amount" => {
+                    new_amout = Bill::get_amount();
+                    match &value.name {
+                        Some(inner_name) => {
+                            *value = Bill::create_bill(Some(inner_name.to_owned()), new_amout);
+                        }
+                        None => {
+                            *value = Bill::create_bill(None, new_amout);
+                        }
+                    }
+                    break;
+                }
+                _ => {
+                    println!("only name/amount can be edited. please enter name/amount:");
+                    continue;
+                }
+            },
+            Err(e) => {
+                println!("error: {:?}", e);
+                continue;
+            }
+        }
+    }
+    edited_hashmap
 }
 
 fn main() {
@@ -182,7 +276,12 @@ fn main() {
         bills.insert(generate_id(), Bill::create_bill(name, amount));
         start = continue_input("generate bill");
     }
-    println!("current bills: ");
     print_hashmap(&bills);
-    delete_bills(&mut bills);
+    if bills.len() > 0 {
+        delete_bills(&mut bills)
+    };
+    if bills.len() > 0 {
+        let edited_bills: HashMap<u32, Bill> = edit_bills(&mut bills);
+        print_hashmap(&edited_bills);
+    };
 }
