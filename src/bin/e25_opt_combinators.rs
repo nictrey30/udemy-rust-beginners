1d58bdac154bf7418237881351f1dab759d531f7
Niculescu Traian <traian30y@yahoo.co.uk>
2024-01-01 23:04:36 +0000

    e25 option combinators

--- src/bin/e25_opt_combinators.rs
+++ src/bin/e25_opt_combinators.rs
@@ -1,32 +1,28 @@
+#![allow(dead_code, unused_variables, unused_imports)]
 // Topic: Option combinators
-//
 // Requirements:
 // * Use combinators as described in the functions:
 //   part_1, part_2, and part_3
-//
 // Notes:
 // * Run `cargo test --bin a23` to check your program.
 // * Only edit the part_1, part_2, and part_3 functions.
 
-fn part_1() -> bool {
-    // We are checking whether or not this particular user
-    // has an access level. The "admin" user does have
-    // an access level.
+fn part_1(name: &str) -> bool {
+    // We are checking whether or not this particular user has an access level. The "admin" user does have an access level.
     // Note: Use is_some or is_none.
-    maybe_access("admin")
+    maybe_access(name).is_some()
 }
 
 fn part_2() -> Option<Access> {
-    // "Root" is equivalent to Access::Admin, but it is
-    // not listed in the maybe_access function.
+    // "Root" is equivalent to Access::Admin, but it is not listed in the maybe_access function.
     // Note: Use or_else and root().
-    maybe_access("root")
+    maybe_access("root").or_else(|| Some(Access::Admin))
 }
 
 fn part_3() -> Access {
     // "Alice" is not a listed user, so she will be a guest.
     // Note: Use unwrap_or_else.
-    maybe_access("Alice")
+    maybe_access("Alice").unwrap_or_else(|| Access::Guest)
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -55,8 +51,20 @@ mod test {
     use crate::*;
 
     #[test]
-    fn check_part_1() {
-        assert_eq!(part_1(), true, "Admins have an access level");
+    fn check_part_1_admin() {
+        assert_eq!(part_1("admin"), true, "Admins have an access level");
+    }
+    #[test]
+    fn check_part_1_users() {
+        assert_eq!(part_1("gary"), true, "Users have an access level");
+    }
+    #[test]
+    fn check_part_1_outsiders() {
+        assert_eq!(
+            part_1("gigi"),
+            false,
+            "outsiders doesn't have an access level"
+        );
     }
 
     #[test]
