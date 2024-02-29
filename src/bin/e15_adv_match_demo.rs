c115221ce2c7dab4fef7e1c2436f04cd584430f9
Traian <traian30y@yahoo.co.uk>
2024-02-29 19:40:08 +0000

    e15_ad_match_demo

--- src/bin/e15_adv_match_demo.rs
+++ src/bin/e15_adv_match_demo.rs
@@ -8,4 +8,22 @@ struct Ticket {
     price: i32,
 }
 
-fn main() {}
+fn main() {
+    let flat = Discount::Flat(2);
+    match flat {
+        Discount::Flat(2) => println!("flat discount of 2"),
+        Discount::Flat(amount) => println!("flat discount of {amount}"),
+        // _ means ignore everything else
+        _ => (),
+    }
+    let concert = Ticket {
+        event: "concert".to_owned(),
+        price: 50,
+    };
+    match concert {
+        // match only when the price is 50
+        Ticket { price: 50, event } => println!("event @ 50 = {:?}", event),
+        // .. means any other fields ignore them
+        Ticket { price, .. } => println!("price = {:?}", price),
+    }
+}
