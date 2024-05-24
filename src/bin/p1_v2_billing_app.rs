f05b2dfdecf92c5b01540576ea03a2a6ae88cf83
Traian <traian30y@yahoo.co.uk>
2024-05-24 20:34:05 +0100

    real world p1 may24

---
+++ src/bin/p1_v2_billing_app.rs
@@ -0,0 +1,17 @@
+// Project 1: Interactive bill manager
+// User stories:
+// * L1: I want to add bills, including the name and amount owed. L1: I want to view existing bills.
+// * L2: I want to remove bills.
+// * L3: I want to edit existing bills. L3: I want to go back if I change my mind.
+// Tips:
+// * Use the loop keyword to create an interactive menu.
+// * Each menu choice should be it's own function, so you can work on the the functionality for that menu in isolation.
+// * A vector is the easiest way to store the bills at level 1, but a hashmap will be easier to work with at levels 2 and 3.
+// * Create a function just for retrieving user input, and reuse it throughout your program.
+
+struct Bill {
+    name: String,
+    amount: f64,
+}
+
+fn main() {}
